e10706b6c167454a723636e0929061201a2f461b
David Aguilar <davvid@gmail.com>
2008-05-29 17:47:53 -0700

    git_python: add utils.pop_key to __init__.py
    This keeps the interface consistent by importing
    utils.pop_key into the git_python namespace.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- lib/git_python/__init__.py
+++ lib/git_python/__init__.py
@@ -11,7 +11,9 @@ from git_python.repo import Repo
 from git_python.stats import Stats
 from git_python.tag import Tag
 from git_python.tree import Tree
-from git_python.utils import dashify, touch
+from git_python.utils import dashify
+from git_python.utils import touch
+from git_python.utils import pop_key
 
 __all__ = [ name for name, obj in locals().items()
             if not (name.startswith('_') or inspect.ismodule(obj)) ]
