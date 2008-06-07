24effa4861d6d1e8cffe848ae63fa2ed40be03f6
Florian Apolloner <florian@apolloner.eu>
2008-06-07 11:36:57 +0200

    fixed up the getattr stuff

--- lib/git/method_missing.py
+++ lib/git/method_missing.py
@@ -5,16 +5,13 @@ class MethodMissingMixin(object):
     This was `taken from a blog post <http://blog.iffy.us/?p=43>`_
     """
     def __getattr__(self, attr):
-        try:
-            return object.__getattribute__(self, attr)
-        except:
-            class MethodMissing(object):
-                def __init__(self, wrapped, method):
-                    self.__wrapped__ = wrapped
-                    self.__method__ = method
-                def __call__(self, *args, **kwargs):
-                    return self.__wrapped__.method_missing(self.__method__, *args, **kwargs)
-            return MethodMissing(self, attr)
+        class MethodMissing(object):
+            def __init__(self, wrapped, method):
+                self.__wrapped__ = wrapped
+                self.__method__ = method
+            def __call__(self, *args, **kwargs):
+                return self.__wrapped__.method_missing(self.__method__, *args, **kwargs)
+        return MethodMissing(self, attr)
 
     def method_missing(self, *args, **kwargs):
         """ This method should be overridden in the derived class. """
