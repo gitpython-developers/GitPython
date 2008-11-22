5da34fddda2ea6de19ecf04efd75e323c4bb41e4
Paul Sowden <paul@idontsmoke.co.uk>
2008-11-21 23:06:19 -0800

    add support for parsing rename info to the diff parser
    Currently the parsed headers are just ignored but I'll add them to the Diff object in a future commit
    (cherry picked from commit 711b655f29b42821c51be8e592143c7db31ed140)

--- lib/git/diff.py
+++ lib/git/diff.py
@@ -39,8 +39,11 @@ class Diff(object):
         diff_header = re.compile(r"""
             #^diff[ ]--git
                 [ ]a/(?P<a_path>\S+)[ ]b/(?P<b_path>\S+)\n
-            (?:^old[ ]mode[ ](?P<old_mode>\d+)(?:\n|$))?
-            (?:^new[ ]mode[ ](?P<new_mode>\d+)(?:\n|$))?
+            (?:^similarity[ ]index[ ](?P<similarity_index>\d+)%\n
+               ^rename[ ]from[ ](?P<rename_from>\S+)\n
+               ^rename[ ]to[ ](?P<rename_to>\S+)(?:\n|$))?
+            (?:^old[ ]mode[ ](?P<old_mode>\d+)\n
+               ^new[ ]mode[ ](?P<new_mode>\d+)(?:\n|$))?
             (?:^new[ ]file[ ]mode[ ](?P<new_file_mode>.+)(?:\n|$))?
             (?:^deleted[ ]file[ ]mode[ ](?P<deleted_file_mode>.+)(?:\n|$))?
             (?:^index[ ](?P<a_commit>[0-9A-Fa-f]+)
@@ -50,7 +53,8 @@ class Diff(object):
         for diff in ('\n' + text).split('\ndiff --git')[1:]:
             header = diff_header(diff)
 
-            a_path, b_path, old_mode, new_mode, new_file_mode, deleted_file_mode, \
+            a_path, b_path, similarity_index, rename_from, rename_to, \
+				old_mode, new_mode, new_file_mode, deleted_file_mode, \
                 a_commit, b_commit, b_mode = header.groups()
             new_file, deleted_file = bool(new_file_mode), bool(deleted_file_mode)
 
