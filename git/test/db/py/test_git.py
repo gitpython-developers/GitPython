09a11c78950ce8e6376ab7a34cdaec77555c6679
Sebastian Thiel <byronimo@gmail.com>
2011-05-30 17:55:04 +0200

    Fixed test_git.py

--- git/test/db/py/test_git.py
+++ git/test/db/py/test_git.py
@@ -2,22 +2,26 @@
 #
 # This module is part of GitDB and is released under
 # the New BSD License: http://www.opensource.org/licenses/bsd-license.php
-from lib import *
+from git.test.lib import rorepo_dir
+from git.test.db.lib import *
 from git.exc import BadObject
-from git.db.py import PureGitODB
+from git.db.py.complex import PureGitODB
 from git.base import OStream, OInfo
 from git.util import hex_to_bin, bin_to_hex
-		
+
+import os
+
 class TestGitDB(TestDBBase):
+	RepoCls = PureGitODB
 	
 	def test_reading(self):
-		gdb = PureGitODB(fixture_path('../../../.git/objects'))
+		gdb = self.RepoCls(os.path.join(rorepo_dir(), 'objects'))
 		
 		# we have packs and loose objects, alternates doesn't necessarily exist
 		assert 1 < len(gdb.databases()) < 4
 		
 		# access should be possible
-		git_sha = hex_to_bin("5690fd0d3304f378754b23b098bd7cb5f4aa1976")
+		git_sha = hex_to_bin("5aebcd5cb3340fb31776941d7e4d518a712a8655")
 		assert isinstance(gdb.info(git_sha), OInfo)
 		assert isinstance(gdb.stream(git_sha), OStream)
 		assert gdb.size() > 200
@@ -29,10 +33,10 @@ class TestGitDB(TestDBBase):
 		# have a separate test module
 		# test partial shas
 		# this one as uneven and quite short
-		assert gdb.partial_to_complete_sha_hex('155b6') == hex_to_bin("155b62a9af0aa7677078331e111d0f7aa6eb4afc")
+		assert gdb.partial_to_complete_sha_hex('5aebcd') == hex_to_bin("5aebcd5cb3340fb31776941d7e4d518a712a8655")
 		
 		# mix even/uneven hexshas
-		for i, binsha in enumerate(sha_list):
+		for i, binsha in enumerate(sha_list[:50]):
 			assert gdb.partial_to_complete_sha_hex(bin_to_hex(binsha)[:8-(i%2)]) == binsha
 		# END for each sha
 		
@@ -40,7 +44,7 @@ class TestGitDB(TestDBBase):
 		
 	@with_rw_directory
 	def test_writing(self, path):
-		gdb = PureGitODB(path)
+		gdb = self.RepoCls(path)
 		
 		# its possible to write objects
 		self._assert_object_writing(gdb)
