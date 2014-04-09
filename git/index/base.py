3a1e0d7117b9e4ea4be3ef4895e8b2b4937ff98a
firm1 <dralliw@users.noreply.github.com>
2014-04-09 16:03:21 +0200

    fix syntax error

--- git/index/base.py
+++ git/index/base.py
@@ -873,7 +873,7 @@ class IndexFile(LazyMixin, diff.Diffable, Serializable):
 
         return out
 
-    def def commit(self, message, parent_commits=None, head=True, author=None, committer=None):
+    def commit(self, message, parent_commits=None, head=True, author=None, committer=None):
         """Commit the current default index file, creating a commit object.
 
         For more information on the arguments, see tree.commit.
