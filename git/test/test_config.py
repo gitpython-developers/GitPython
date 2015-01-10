bfce49feb0be6c69f7fffc57ebdd22b6da241278
Sebastian Thiel <byronimo@gmail.com>
2015-01-10 14:09:58 +0100

    rewrote test-string to have a chance to make flake8 happy.
    That way, flake won't have to completely ignore issues it might have
    in other portions of this file.

--- git/test/test_config.py
+++ git/test/test_config.py
@@ -3,8 +3,6 @@
 #
 # This module is part of GitPython and is released under
 # the BSD License: http://www.opensource.org/licenses/bsd-license.php
-# The test test_multi_line_config requires whitespace (especially tabs) to remain
-# flake8: noqa
 
 from git.test.lib import (
     TestCase,
@@ -78,14 +76,15 @@ class TestBase(TestCase):
     def test_multi_line_config(self):
         file_obj = self._to_memcache(fixture_path("git_config_with_comments"))
         config = GitConfigParser(file_obj, read_only=False)
-        ev = r"""ruby -e '
-		system %(git), %(merge-file), %(--marker-size=%L), %(%A), %(%O), %(%B)
-		b = File.read(%(%A))
-		b.sub!(/^<+ .*\nActiveRecord::Schema\.define.:version => (\d+). do\n=+\nActiveRecord::Schema\.define.:version => (\d+). do\n>+ .*/) do
-		  %(ActiveRecord::Schema.define(:version => #{[$1, $2].max}) do)
-		end
-		File.open(%(%A), %(w)) {|f| f.write(b)}
-		exit 1 if b.include?(%(<)*%L)'"""
+        ev = "ruby -e '\n"
+        ev += "		system %(git), %(merge-file), %(--marker-size=%L), %(%A), %(%O), %(%B)\n"
+        ev += "		b = File.read(%(%A))\n"
+        ev += "		b.sub!(/^<+ .*\\nActiveRecord::Schema\\.define.:version => (\\d+). do\\n=+\\nActiveRecord::Schema\\."
+        ev += "define.:version => (\\d+). do\\n>+ .*/) do\n"
+        ev += "		  %(ActiveRecord::Schema.define(:version => #{[$1, $2].max}) do)\n"
+        ev += "		end\n"
+        ev += "		File.open(%(%A), %(w)) {|f| f.write(b)}\n"
+        ev += "		exit 1 if b.include?(%(<)*%L)'"
         assert_equal(config.get('merge "railsschema"', 'driver'), ev)
         assert_equal(config.get('alias', 'lg'),
                      "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset'"
