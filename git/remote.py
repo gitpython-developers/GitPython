08938d6cee0dc4b45744702e7d0e7f74f2713807
Vincent Driessen <me@nvie.com>
2016-04-20 00:29:19 +0200

    Allow "$" sign in fetch output lines

--- git/remote.py
+++ git/remote.py
@@ -186,7 +186,7 @@ class FetchInfo(object):
         FAST_FORWARD, ERROR = [1 << x for x in range(8)]
 
     #                             %c    %-*s %-*s             -> %s       (%s)
-    re_fetch_result = re.compile("^\s*(.) (\[?[\w\s\.]+\]?)\s+(.+) -> ([/\w_\+\.\-#]+)(    \(.*\)?$)?")
+    re_fetch_result = re.compile("^\s*(.) (\[?[\w\s\.$]+\]?)\s+(.+) -> ([/\w_\+\.\-$#]+)(    \(.*\)?$)?")
 
     _flag_map = {'!': ERROR,
                  '+': FORCED_UPDATE,
