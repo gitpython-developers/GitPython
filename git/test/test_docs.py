df65f51de6ba67138a48185ff2e63077f7fe7ce6
Forrest Hopkins <fingerheroes@gmail.com>
2016-08-23 22:45:52 -0700

    Update Remotes section (#502)
    Update Remotes section
    
    The Remotes section was missing some pretty important info.

--- git/test/test_docs.py
+++ git/test/test_docs.py
@@ -373,8 +373,13 @@ class Tutorials(TestBase):
         assert origin == empty_repo.remotes.origin == empty_repo.remotes['origin']
         origin.fetch()                  # assure we actually have data. fetch() returns useful information
         # Setup a local tracking branch of a remote branch
-        empty_repo.create_head('master', origin.refs.master).set_tracking_branch(origin.refs.master)
-        origin.rename('new_origin')   # rename remotes
+        empty_repo.create_head('master', origin.refs.master)  # create local branch "master" from remote branch "master"
+        empty_repo.heads.master.set_tracking_branch(origin.refs.master)  # set local "master" to track remote "master
+        empty_repo.heads.master.checkout()  # checkout local "master" to working tree
+        # Three above commands in one:
+        empty_repo.create_head('master', origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
+        # rename remotes
+        origin.rename('new_origin')
         # push and pull behaves similarly to `git push|pull`
         origin.pull()
         origin.push()
