dac619e4917b0ad43d836a534633d68a871aecca
Sebastian Thiel <sthiel@thoughtworks.com>
2019-07-20 17:26:16 +0800

    Drop python 2.7 support and help with encodings
    Fixes #312

--- git/repo/base.py
+++ git/repo/base.py
@@ -4,6 +4,7 @@
 # This module is part of GitPython and is released under
 # the BSD License: http://www.opensource.org/licenses/bsd-license.php
 
+from builtins import str
 from collections import namedtuple
 import logging
 import os
