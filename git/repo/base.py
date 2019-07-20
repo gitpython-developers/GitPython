cc664f07535e3b3c1884d0b7f3cbcbadf9adce25
Sebastian Thiel <sthiel@thoughtworks.com>
2019-07-20 15:32:05 +0800

    Revert "Merge branch 'PR-non-ascii-filenames' of https://github.com/xarx00/GitPython into xarx00-PR-non-ascii-filenames"
    This reverts commit 3b13c115994461fb6bafe5dd06490aae020568c1, reversing
    changes made to da8aeec539da461b2961ca72049df84bf30473e1.
    
    It doesn't pass, unfortunately. Is it a travis issue?
    https://travis-ci.org/gitpython-developers/GitPython/jobs/561333763#L340

--- git/repo/base.py
+++ git/repo/base.py
@@ -4,7 +4,6 @@
 # This module is part of GitPython and is released under
 # the BSD License: http://www.opensource.org/licenses/bsd-license.php
 
-from builtins import str
 from collections import namedtuple
 import logging
 import os
