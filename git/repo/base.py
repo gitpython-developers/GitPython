fb2461d84f97a72641ef1e878450aeab7cd17241
Robert Westman <robert@byteflux.io>
2021-06-05 12:35:41 +0200

    Removes unused import

--- git/repo/base.py
+++ git/repo/base.py
@@ -3,7 +3,6 @@
 #
 # This module is part of GitPython and is released under
 # the BSD License: http://www.opensource.org/licenses/bsd-license.php
-import binascii
 import logging
 import os
 import re
