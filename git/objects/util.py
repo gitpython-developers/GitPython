2fe13cad9c889b8628119ab5ee139038b0c164fd
Yobmod <yobmod@gmail.com>
2021-08-02 16:54:31 +0100

    Fix parse_date typing 3

--- git/objects/util.py
+++ git/objects/util.py
@@ -181,13 +181,11 @@ def parse_date(string_date: Union[str, datetime]) -> Tuple[int, int]:
     :raise ValueError: If the format could not be understood
     :note: Date can also be YYYY.MM.DD, MM/DD/YYYY and DD.MM.YYYY.
     """
-    if isinstance(string_date, datetime):
-        if string_date.tzinfo and string_date.utcoffset():
-            utcoffset = string_date.utcoffset()
-            offset = -int(utcoffset.total_seconds()) if utcoffset else 0
-        else:
-            offset = 0
+    if isinstance(string_date, datetime) and string_date.tzinfo:
+        utcoffset = string_date.utcoffset()
+        offset = -int(utcoffset.total_seconds()) if utcoffset else 0
         return int(string_date.astimezone(utc).timestamp()), offset
+
     else:
         assert isinstance(string_date, str), f"string_date={string_date}, type={type(string_date)}"  # for mypy
 
