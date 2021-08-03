9c7a44fddc3d69781eab975b2b58c2e338092116
Eric Wieser <wieser.eric@gmail.com>
2021-08-03 17:12:49 +0100

    Fix trailing whitespace and incorrect overload

--- git/config.py
+++ git/config.py
@@ -710,12 +710,12 @@ class GitConfigParser(cp.RawConfigParser, metaclass=MetaParserBuilder):
         return self._read_only
 
     @overload
-    def get_value(self, section: str, option: str, default: None = None) -> str: ...
+    def get_value(self, section: str, option: str, default: None = None) -> Union[int, float, str, bool]: ...
 
     @overload
     def get_value(self, section: str, option: str, default: str) -> str: ...
 
-    @overload 
+    @overload
     def get_value(self, section: str, option: str, default: float) -> float: ...
 
     def get_value(self, section: str, option: str, default: Union[int, float, str, bool, None] = None
