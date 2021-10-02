d074375190ddadfd92da4e7606d93829b072c20a
Trym Bremnes <trym.bremnes@gmail.com>
2021-10-02 16:42:35 +0200

    Revert "Replace wildcard imports with concrete imports"
    This reverts commit 53d94b8091b36847bb9e495c76bb5a3ec2a2fdb5.
    
    The reason for the revert is that the commit in question introduced a
    regression where certain modules, functions and classes that were
    exposed before were no longer exposed.
    
    See https://github.com/gitpython-developers/GitPython/pull/1352#issuecomment-932757204
    for additional information.

--- test/lib/__init__.py
+++ test/lib/__init__.py
@@ -4,12 +4,9 @@
 # This module is part of GitPython and is released under
 # the BSD License: http://www.opensource.org/licenses/bsd-license.php
 
+# flake8: noqa
 import inspect
-
-from .helper import (GIT_DAEMON_PORT, SkipTest, StringProcessAdapter, TestBase,
-                     TestCase, fixture, fixture_path,
-                     with_rw_and_rw_remote_repo, with_rw_directory,
-                     with_rw_repo)
+from .helper import *
 
 __all__ = [name for name, obj in locals().items()
            if not (name.startswith('_') or inspect.ismodule(obj))]
