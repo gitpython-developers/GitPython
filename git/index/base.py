gitpython-developers/GitPython

commit d79d20d28b1f9324193309cffd2ab79e0edae925

Avoid taking a lock for reading
This isn't needed as git will replace this file atomicially,
hence we always see a fully written file when reading.

Only when writing we need to obtain a lock.

git/index/base.py
@@ -127,30 +127,17 @@ class IndexFile(LazyMixin, git_diff.Diffable, Serializable):
 
     def _set_cache_(self, attr: str) -> None:
         if attr == "entries":
-            # read the current index
-            # try memory map for speed
-            lfd = LockedFD(self._file_path)
-            ok = False
             try:
-                fd = lfd.open(write=False, stream=False)
-                ok = True
+                fd = os.open(self._file_path, os.O_RDONLY)
             except OSError:
                 # in new repositories, there may be no index, which means we are empty
                 self.entries: Dict[Tuple[PathLike, StageType], IndexEntry] = {}
                 return None
-            finally:
-                if not ok:
-                    lfd.rollback()
             # END exception handling
 
             stream = file_contents_ro(fd, stream=True, allow_mmap=True)
 
-            try:
-                self._deserialize(stream)
-            finally:
-                lfd.rollback()
-                # The handles will be closed on destruction
-            # END read from default index on demand
+            self._deserialize(stream)
         else:
             super(IndexFile, self)._set_cache_(attr)
 

commit e16a0040d07afa4ac9c0548aa742ec18ec1395a8

Assure index file descriptor is closed after reader (#1394) (#1395)
A regression that was introduced with d79d20d.

git/index/base.py
@@ -135,7 +135,10 @@ class IndexFile(LazyMixin, git_diff.Diffable, Serializable):
                 return None
             # END exception handling
 
-            stream = file_contents_ro(fd, stream=True, allow_mmap=True)
+            try:
+                stream = file_contents_ro(fd, stream=True, allow_mmap=True)
+            finally:
+                os.close(fd)
 
             self._deserialize(stream)
         else: