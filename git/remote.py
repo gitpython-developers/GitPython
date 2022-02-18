75f4f63ab3856a552f06082aabf98845b5fa21e3
theworstcomrade <4lbercik@gmail.com>
2022-02-18 16:28:03 +0100

    Low risk ReDoS vuln
    https://huntr.dev/bounties/8549d81f-dc45-4af7-9f2a-2d70752d8524/

--- git/remote.py
+++ git/remote.py
@@ -273,7 +273,7 @@ class FetchInfo(IterableObj, object):
     NEW_TAG, NEW_HEAD, HEAD_UPTODATE, TAG_UPDATE, REJECTED, FORCED_UPDATE, \
         FAST_FORWARD, ERROR = [1 << x for x in range(8)]
 
-    _re_fetch_result = re.compile(r'^\s*(.) (\[?[\w\s\.$@]+\]?)\s+(.+) -> ([^\s]+)(    \(.*\)?$)?')
+    _re_fetch_result = re.compile(r'^\s*(.) (\[[\w\s\.$@]+\]|[\w\.$@]+)\s+(.+) -> ([^\s]+)(    \(.*\)?$)?')
 
     _flag_map: Dict[flagKeyLiteral, int] = {
         '!': ERROR,
