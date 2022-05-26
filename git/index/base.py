27a915b8976d9cc981fbde690ce42b5c43250914
whokilleddb <whokilleddb@gmail.com>
2022-05-26 16:11:07 +0530

    Safer Temporary Files

--- git/index/base.py
+++ git/index/base.py
@@ -351,7 +351,7 @@ class IndexFile(LazyMixin, git_diff.Diffable, Serializable):
 
         # tmp file created in git home directory to be sure renaming
         # works - /tmp/ dirs could be on another device
-        tmp_index = tempfile.mktemp("", "", repo.git_dir)
+        tmp_index = tempfile.mkstemp("", "", dir=repo.git_dir)
         arg_list.append("--index-output=%s" % tmp_index)
         arg_list.extend(treeish)
 
