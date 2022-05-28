32785d32972a325aea32ee4e6e5cac5cd8f18447
whokilleddb <whokilleddb@gmail.com>
2022-05-28 18:54:59 +0530

    Fixed Tempfiles

--- git/index/util.py
+++ git/index/util.py
@@ -40,7 +40,7 @@ class TemporaryFileSwap(object):
 
     def __init__(self, file_path: PathLike) -> None:
         self.file_path = file_path
-        self.tmp_file_path = tempfile.NamedTemporaryFile(dir=str(self.file_path), delete=False).name
+        self.tmp_file_path = str(self.file_path) + osp.basename(tempfile.mkstemp("", "", "")[1])
         # it may be that the source does not exist
         try:
             os.rename(self.file_path, self.tmp_file_path)
