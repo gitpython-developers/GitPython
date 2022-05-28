320523a429b70abab4db28533547c2d70e8b7bc7
whokilleddb <whokilleddb@gmail.com>
2022-05-28 09:31:24 +0530

    Fixed Minor Bugs

--- git/index/util.py
+++ git/index/util.py
@@ -40,7 +40,7 @@ class TemporaryFileSwap(object):
 
     def __init__(self, file_path: PathLike) -> None:
         self.file_path = file_path
-        self.tmp_file_path = tempfile.mktemp("", "", dir=str(self.file_path))[1]
+        self.tmp_file_path = tempfile.mktemp("", "", dir=str(self.file_path))
         # it may be that the source does not exist
         try:
             os.rename(self.file_path, self.tmp_file_path)
