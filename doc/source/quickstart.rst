6a9154b1bfcebe7ee28edebec6617993ad6a5569
LeoDaCoda <LeoDaCoda@gmail.com>
2023-07-08 20:52:04 -0400

    Added git clone & git add

--- doc/source/quickstart.rst
+++ doc/source/quickstart.rst
@@ -16,6 +16,8 @@ There are a few ways to create a :class:`git.Repo <git.repo.base.Repo>` object
 An existing local path
 ######################
 
+$ git init path/to/dir
+
 .. literalinclude:: ../../test/test_quick_doc.py
     :language: python
     :dedent: 8
@@ -34,4 +36,27 @@ Existing local git Repo
 Clone from URL
 ##############
 
-For the rest of this tutorial we will use a clone from https://github.com
+For the rest of this tutorial we will use a clone from https://github.com/LeoDaCoda/GitPython-TestFileSys.git
+
+git clone https://some_repo_url
+
+.. literalinclude:: ../../test/test_quick_doc.py
+    :language: python
+    :dedent: 8
+    :start-after: # [1-test_cloned_repo_object]
+    :end-before: # ![1-test_cloned_repo_object]
+
+Usage
+****************
+
+* git add filepath
+
+
+
+
+* git commit -m message
+* git log file
+* git status
+
+
+
--- test/test_quick_doc.py
+++ test/test_quick_doc.py
@@ -1,6 +1,6 @@
 import pytest
 
-import git
+
 from test.lib import TestBase
 from test.lib.helper import with_rw_directory
 
@@ -18,16 +18,46 @@ class QuickDoc(TestBase):
         # [1-test_init_repo_object]
         from git import Repo
 
-        repo = Repo.init(path_to_dir)
-        assert repo.__class__ is Repo # Test to confirm repo was initialized
+        repo = Repo.init(path_to_dir)  # git init path/to/dir
+        assert repo.__class__ is Repo  # Test to confirm repo was initialized
         # ![1-test_init_repo_object]
 
         # [2-test_init_repo_object]
+        import git
+
         try:
             repo = Repo(path_to_dir)
         except git.NoSuchPathError:
             assert False, f"No such path {path_to_dir}"
-        # ! [2-test_init_repo_object]
+        # ![2-test_init_repo_object]
 
-        # [3 - test_init_repo_object]
+    @with_rw_directory
+    def test_cloned_repo_object(self, rw_dir):
+        local_dir = rw_dir
+
+        from git import Repo
+        import git
+        # code to clone from url
+        # [1-test_cloned_repo_object]
+        repo_url = "https://github.com/LeoDaCoda/GitPython-TestFileSys.git"
+
+        try:
+            repo = Repo.clone_from(repo_url, local_dir)
+        except git.CommandError:
+            assert False, f"Invalid address {repo_url}"
+        # ![1-test_cloned_repo_object]
+
+        # code to add files
+        # [2-test_cloned_repo_object]
+        # We must make a change to a file so that we can add the update to git
+
+        update_file = 'dir1/file2.txt' # we'll use /dir1/file2.txt
+        with open(f"{local_dir}/{update_file}", 'a') as f:
+            f.write('\nUpdate version 2')
+        # ![2-test_cloned_repo_object]
+
+        # [3-test_cloned_repo_object]
+        add_file = [f"{local_dir}/{update_file}"]
+        repo.index.add(add_file)  # notice the add function requires a list of paths
+        # [3-test_cloned_repo_object]
 
