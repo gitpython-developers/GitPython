0920371905561d7a242a8be158b79d1a8408a7c4
Eliah Kagan <degeneracypressure@gmail.com>
2023-09-27 04:40:08 -0400

    Extract suggest_venv out of the else block
    I think this is easier to read, but this is admittedly subjective.
    
    This commit also makes the separate change of adjusting comment
    spacing for consistency within the script. (Two spaces before "#"
    is not widely regarded as better than one in shell scripting, so
    unlike Python where PEP-8 recommends that, it would be equally good
    to have changed all the other places in the shell scrips to have
    just one.)

--- build-release.sh
+++ build-release.sh
@@ -9,6 +9,11 @@ function release_with() {
     "$1" -m build --sdist --wheel
 }
 
+function suggest_venv() {
+    local venv_cmd='python -m venv env && source env/bin/activate'
+    printf "HELP: To avoid this error, use a virtual-env with '%s' instead.\n" "$venv_cmd"
+}
+
 if test -n "${VIRTUAL_ENV:-}"; then
     deps=(build twine)  # Install twine along with build, as we need it later.
     echo "Virtual environment detected. Adding packages: ${deps[*]}"
@@ -16,11 +21,7 @@ if test -n "${VIRTUAL_ENV:-}"; then
     echo 'Starting the build.'
     release_with python
 else
-    function suggest_venv() {
-        venv_cmd='python -m venv env && source env/bin/activate'
-        printf "HELP: To avoid this error, use a virtual-env with '%s' instead.\n" "$venv_cmd"
-    }
     trap suggest_venv ERR  # This keeps the original exit (error) code.
     echo 'Starting the build.'
-    release_with python3 # Outside a venv, use python3.
+    release_with python3  # Outside a venv, use python3.
 fi
