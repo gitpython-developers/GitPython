e604b469a1bdfb83f03d4c2ef1f79b45b8a5c3fa
Eliah Kagan <degeneracypressure@gmail.com>
2023-09-27 05:44:02 -0400

    Use 4-space indentation in all shell scripts
    This had been done everywhere except in init-tests-after-clone.sh.

--- init-tests-after-clone.sh
+++ init-tests-after-clone.sh
@@ -3,10 +3,10 @@
 set -e
 
 if [[ -z "$TRAVIS" ]]; then
-  read -rp "This operation will destroy locally modified files. Continue ? [N/y]: " answer
-  if [[ ! $answer =~ [yY] ]]; then
-    exit 2
-  fi
+    read -rp "This operation will destroy locally modified files. Continue ? [N/y]: " answer
+    if [[ ! $answer =~ [yY] ]]; then
+        exit 2
+    fi
 fi
 
 git tag __testing_point__
