2c94b6aee17393ffdf8785bafb31e54e854e6882
Eliah Kagan <degeneracypressure@gmail.com>
2023-10-10 01:44:25 -0400

    Remove r prefix from strings that need not be raw
    These are a couple strings that, in addition to not having any
    escape sequences, don't represent regular expressions, Windows
    paths, or anything else that would be clarified by raw literals.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -170,7 +170,7 @@ htmlhelp_basename = "gitpythondoc"
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, document class [howto/manual]).
 latex_documents = [
-    ("index", "GitPython.tex", r"GitPython Documentation", r"Michael Trier", "manual"),
+    ("index", "GitPython.tex", "GitPython Documentation", "Michael Trier", "manual"),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
