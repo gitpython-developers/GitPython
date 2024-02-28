gitpython-developers/GitPython

commit cd16a3534eedb5a0990293e16a6371ca16d8776a

Revise docstrings and comments for clarity and formatting
In the git module (including the modules it contains).

This also makes one small change in doc/ to synchronize with a
change made in a docstring.

git/objects/tag.py
@@ -3,7 +3,9 @@
 #
 # This module is part of GitPython and is released under
 # the BSD License: https://opensource.org/license/bsd-3-clause/
-""" Module containing all object based types. """
+
+"""Module containing all Object-based types."""
+
 from . import base
 from .util import get_object_type_by_name, parse_actor_and_date
 from ..util import hex_to_bin
@@ -24,10 +26,10 @@ __all__ = ("TagObject",)
 
 
 class TagObject(base.Object):
-
-    """Non-Lightweight tag carrying additional information about an object we are pointing to."""
+    """Non-lightweight tag carrying additional information about an object we are pointing to."""
 
     type: Literal["tag"] = "tag"
+
     __slots__ = (
         "object",
         "tag",
@@ -48,18 +50,20 @@ class TagObject(base.Object):
         tagger_tz_offset: Union[int, None] = None,
         message: Union[str, None] = None,
     ) -> None:  # @ReservedAssignment
-        """Initialize a tag object with additional data
+        """Initialize a tag object with additional data.
 
-        :param repo: repository this object is located in
+        :param repo: Repository this object is located in
         :param binsha: 20 byte SHA1
         :param object: Object instance of object we are pointing to
-        :param tag: name of this tag
+        :param tag: Name of this tag
         :param tagger: Actor identifying the tagger
         :param tagged_date: int_seconds_since_epoch
-            is the DateTime of the tag creation - use time.gmtime to convert
-            it into a different format
-        :param tagged_tz_offset: int_seconds_west_of_utc is the timezone that the
-            authored_date is in, in a format similar to time.altzone"""
+            The :class:`DateTime` of the tag creation.
+            Use :func:`time.gmtime` to convert it into a different format.
+        :param tagged_tz_offset: int_seconds_west_of_utc
+            The timezone that the authored_date is in, in a format similar
+            to :attr:`time.altzone`.
+        """
         super(TagObject, self).__init__(repo, binsha)
         if object is not None:
             self.object: Union["Commit", "Blob", "Tree", "TagObject"] = object
@@ -75,7 +79,7 @@ class TagObject(base.Object):
             self.message = message
 
     def _set_cache_(self, attr: str) -> None:
-        """Cache all our attributes at once"""
+        """Cache all our attributes at once."""
         if attr in TagObject.__slots__:
             ostream = self.repo.odb.stream(self.binsha)
             lines: List[str] = ostream.read().decode(defenc, "replace").splitlines()
@@ -95,9 +99,9 @@ class TagObject(base.Object):
                     self.tagger_tz_offset,
                 ) = parse_actor_and_date(tagger_info)
 
-            # line 4 empty - it could mark the beginning of the next header
-            # in case there really is no message, it would not exist. Otherwise
-            # a newline separates header from message
+            # Line 4 empty - it could mark the beginning of the next header.
+            # In case there really is no message, it would not exist.
+            # Otherwise a newline separates header from message.
             if len(lines) > 5:
                 self.message = "\n".join(lines[5:])
             else:

git/refs/tag.py
@@ -18,10 +18,9 @@ if TYPE_CHECKING:
 
 
 class TagReference(Reference):
-
-    """Class representing a lightweight tag reference which either points to a commit
-    ,a tag object or any other object. In the latter case additional information,
-    like the signature or the tag-creator, is available.
+    """A lightweight tag reference which either points to a commit, a tag object or any
+    other object. In the latter case additional information, like the signature or the
+    tag-creator, is available.
 
     This tag object will always point to a commit object, but may carry additional
     information in a tag object::
@@ -29,9 +28,11 @@ class TagReference(Reference):
      tagref = TagReference.list_items(repo)[0]
      print(tagref.commit.message)
      if tagref.tag is not None:
-        print(tagref.tag.message)"""
+        print(tagref.tag.message)
+    """
 
     __slots__ = ()
+
     _common_default = "tags"
     _common_path_default = Reference._common_path_default + "/" + _common_default
 
@@ -39,11 +40,12 @@ class TagReference(Reference):
     def commit(self) -> "Commit":  # type: ignore[override]  # LazyMixin has unrelated commit method
         """:return: Commit object the tag ref points to
 
-        :raise ValueError: if the tag points to a tree or blob"""
+        :raise ValueError: If the tag points to a tree or blob
+        """
         obj = self.object
         while obj.type != "commit":
             if obj.type == "tag":
-                # it is a tag object which carries the commit as an object - we can point to anything
+                # It is a tag object which carries the commit as an object - we can point to anything.
                 obj = obj.object
             else:
                 raise ValueError(
@@ -59,16 +61,13 @@ class TagReference(Reference):
     def tag(self) -> Union["TagObject", None]:
         """
         :return: Tag object this tag ref points to or None in case
-            we are a light weight tag"""
+            we are a lightweight tag"""
         obj = self.object
         if obj.type == "tag":
             return obj
         return None
 
-    # make object read-only
-    # It should be reasonably hard to adjust an existing tag
-
-    # object = property(Reference._get_object)
+    # Make object read-only. It should be reasonably hard to adjust an existing tag.
     @property
     def object(self) -> Commit_ish:  # type: ignore[override]
         return Reference._get_object(self)
@@ -86,30 +85,31 @@ class TagReference(Reference):
         """Create a new tag reference.
 
         :param path:
-            The name of the tag, i.e. 1.0 or releases/1.0.
-            The prefix refs/tags is implied
+            The name of the tag, e.g. ``1.0`` or ``releases/1.0``.
+            The prefix ``refs/tags`` is implied.
 
         :param ref:
-            A reference to the Object you want to tag. The Object can be a commit, tree or
-            blob.
+            A reference to the :class:`Object <git.objects.base.Object>` you want to
+            tag. The Object can be a commit, tree or blob.
 
         :param logmsg:
             If not None, the message will be used in your tag object. This will also
-            create an additional tag object that allows to obtain that information, i.e.::
+            create an additional tag object that allows to obtain that information, e.g.::
 
                 tagref.tag.message
 
         :param message:
-            Synonym for :param logmsg:
-            Included for backwards compatibility. :param logmsg is used in preference if both given.
+            Synonym for the `logmsg` parameter.
+            Included for backwards compatibility. `logmsg` takes precedence if both are passed.
 
         :param force:
-            If True, to force creation of a tag even though that tag already exists.
+            If True, force creation of a tag even though that tag already exists.
 
         :param kwargs:
-            Additional keyword arguments to be passed to git-tag
+            Additional keyword arguments to be passed to git-tag.
 
-        :return: A new TagReference"""
+        :return: A new TagReference.
+        """
         if "ref" in kwargs and kwargs["ref"]:
             reference = kwargs["ref"]
 
@@ -130,9 +130,9 @@ class TagReference(Reference):
 
     @classmethod
     def delete(cls, repo: "Repo", *tags: "TagReference") -> None:  # type: ignore[override]
-        """Delete the given existing tag or tags"""
+        """Delete the given existing tag or tags."""
         repo.git.tag("-d", *tags)
 
 
-# provide an alias
+# Provide an alias.
 Tag = TagReference

git/__init__.py
@@ -3,8 +3,10 @@
 #
 # This module is part of GitPython and is released under
 # the BSD License: https://opensource.org/license/bsd-3-clause/
+
 # flake8: noqa
 # @PydevCodeAnalysisIgnore
+
 from git.exc import *  # @NoMove @IgnorePep8
 from typing import List, Optional, Sequence, Tuple, Union, TYPE_CHECKING
 from git.types import PathLike

git/compat.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
-# config.py
+# compat.py
 # Copyright (C) 2008, 2009 Michael Trier (mtrier@gmail.com) and contributors
 #
 # This module is part of GitPython and is released under
 # the BSD License: https://opensource.org/license/bsd-3-clause/
-"""utilities to help provide compatibility with python 3"""
+
+"""Utilities to help provide compatibility with Python 3."""
+
 # flake8: noqa
 
 import locale
@@ -50,7 +52,7 @@ def safe_decode(s: AnyStr) -> str:
 
 
 def safe_decode(s: Union[AnyStr, None]) -> Optional[str]:
-    """Safely decodes a binary string to unicode"""
+    """Safely decode a binary string to Unicode."""
     if isinstance(s, str):
         return s
     elif isinstance(s, bytes):
@@ -72,7 +74,7 @@ def safe_encode(s: AnyStr) -> bytes:
 
 
 def safe_encode(s: Optional[AnyStr]) -> Optional[bytes]:
-    """Safely encodes a binary string to unicode"""
+    """Safely encode a binary string to Unicode."""
     if isinstance(s, str):
         return s.encode(defenc)
     elif isinstance(s, bytes):
@@ -94,7 +96,7 @@ def win_encode(s: AnyStr) -> bytes:
 
 
 def win_encode(s: Optional[AnyStr]) -> Optional[bytes]:
-    """Encode unicodes for process arguments on Windows."""
+    """Encode Unicode strings for process arguments on Windows."""
     if isinstance(s, str):
         return s.encode(locale.getpreferredencoding(False))
     elif isinstance(s, bytes):

git/db.py
@@ -1,4 +1,5 @@
-"""Module with our own gitdb implementation - it uses the git command"""
+"""Module with our own gitdb implementation - it uses the git command."""
+
 from git.util import bin_to_hex, hex_to_bin
 from gitdb.base import OInfo, OStream
 from gitdb.db import GitDB
@@ -22,17 +23,17 @@ __all__ = ("GitCmdObjectDB", "GitDB")
 
 
 class GitCmdObjectDB(LooseObjectDB):
-
     """A database representing the default git object store, which includes loose
-    objects, pack files and an alternates file
+    objects, pack files and an alternates file.
 
     It will create objects only in the loose object database.
-    :note: for now, we use the git command to do all the lookup, just until he
-        have packs and the other implementations
+
+    :note: For now, we use the git command to do all the lookup, just until we
+        have packs and the other implementations.
     """
 
     def __init__(self, root_path: PathLike, git: "Git") -> None:
-        """Initialize this instance with the root and a git command"""
+        """Initialize this instance with the root and a git command."""
         super(GitCmdObjectDB, self).__init__(root_path)
         self._git = git
 
@@ -48,11 +49,15 @@ class GitCmdObjectDB(LooseObjectDB):
     # { Interface
 
     def partial_to_complete_sha_hex(self, partial_hexsha: str) -> bytes:
-        """:return: Full binary 20 byte sha from the given partial hexsha
+        """
+        :return: Full binary 20 byte sha from the given partial hexsha
+
         :raise AmbiguousObjectName:
         :raise BadObject:
-        :note: currently we only raise BadObject as git does not communicate
-            AmbiguousObjects separately"""
+
+        :note: Currently we only raise :class:`BadObject` as git does not communicate
+            AmbiguousObjects separately.
+        """
         try:
             hexsha, _typename, _size = self._git.get_object_header(partial_hexsha)
             return hex_to_bin(hexsha)

git/diff.py
@@ -50,7 +50,7 @@ Lit_change_type = Literal["A", "D", "C", "M", "R", "T", "U"]
 
 __all__ = ("Diffable", "DiffIndex", "Diff", "NULL_TREE")
 
-# Special object to compare against the empty tree in diffs
+# Special object to compare against the empty tree in diffs.
 NULL_TREE = object()
 
 _octal_byte_re = re.compile(rb"\\([0-9]{3})")
@@ -80,27 +80,28 @@ def decode_path(path: bytes, has_ab_prefix: bool = True) -> Optional[bytes]:
 
 
 class Diffable(object):
-
-    """Common interface for all object that can be diffed against another object of compatible type.
+    """Common interface for all objects that can be diffed against another object of
+    compatible type.
 
     :note:
-        Subclasses require a repo member as it is the case for Object instances, for practical
-        reasons we do not derive from Object."""
+        Subclasses require a repo member as it is the case for Object instances, for
+        practical reasons we do not derive from Object.
+    """
 
     __slots__ = ()
 
-    # standin indicating you want to diff against the index
     class Index(object):
-        pass
+        """Stand-in indicating you want to diff against the index."""
 
     def _process_diff_args(
         self, args: List[Union[str, "Diffable", Type["Diffable.Index"], object]]
     ) -> List[Union[str, "Diffable", Type["Diffable.Index"], object]]:
         """
         :return:
-            possibly altered version of the given args list.
-            Method is called right before git command execution.
-            Subclasses can use it to alter the behaviour of the superclass"""
+            Possibly altered version of the given args list.
+            This method is called right before git command execution.
+            Subclasses can use it to alter the behaviour of the superclass.
+        """
         return args
 
     def diff(
@@ -110,41 +111,47 @@ class Diffable(object):
         create_patch: bool = False,
         **kwargs: Any,
     ) -> "DiffIndex":
-        """Creates diffs between two items being trees, trees and index or an
-        index and the working tree. It will detect renames automatically.
+        """Create diffs between two items being trees, trees and index or an
+        index and the working tree. Detects renames automatically.
 
         :param other:
-            Is the item to compare us with.
-            If None, we will be compared to the working tree.
-            If Treeish, it will be compared against the respective tree
-            If Index ( type ), it will be compared against the index.
-            If git.NULL_TREE, it will compare against the empty tree.
-            It defaults to Index to assure the method will not by-default fail
-            on bare repositories.
+            This the item to compare us with.
+
+            * If None, we will be compared to the working tree.
+            * If :class:`Treeish <git.index.base.Treeish>`, it will be compared against
+              the respective tree.
+            * If :class:`Index <Diffable.Index>`, it will be compared against the index.
+            * If :attr:`git.NULL_TREE`, it will compare against the empty tree.
+            * It defaults to :class:`Index <Diffable.Index>` to assure the method will
+              not by-default fail on bare repositories.
 
         :param paths:
-            is a list of paths or a single path to limit the diff to.
-            It will only include at least one of the given path or paths.
+            This a list of paths or a single path to limit the diff to. It will only
+            include at least one of the given path or paths.
 
         :param create_patch:
-            If True, the returned Diff contains a detailed patch that if applied
-            makes the self to other. Patches are somewhat costly as blobs have to be read
-            and diffed.
+            If True, the returned :class:`Diff` contains a detailed patch that if
+            applied makes the self to other. Patches are somewhat costly as blobs have
+            to be read and diffed.
 
         :param kwargs:
-            Additional arguments passed to git-diff, such as
-            R=True to swap both sides of the diff.
+            Additional arguments passed to git-diff, such as ``R=True`` to swap both
+            sides of the diff.
 
         :return: git.DiffIndex
 
         :note:
-            On a bare repository, 'other' needs to be provided as Index or as
-            as Tree/Commit, or a git command error will occur"""
+            On a bare repository, 'other' needs to be provided
+            as :class:`Index <Diffable.Index>`,
+            or as :class:`Tree <git.objects.tree.Tree>`
+            or :class:`Commit <git.objects.commit.Commit>`, or a git command error will
+            occur.
+        """
         args: List[Union[PathLike, Diffable, Type["Diffable.Index"], object]] = []
-        args.append("--abbrev=40")  # we need full shas
-        args.append("--full-index")  # get full index paths, not only filenames
+        args.append("--abbrev=40")  # We need full shas.
+        args.append("--full-index")  # Get full index paths, not only filenames.
 
-        # remove default '-M' arg (check for renames) if user is overriding it
+        # Remove default '-M' arg (check for renames) if user is overriding it.
         if not any(x in kwargs for x in ("find_renames", "no_renames", "M")):
             args.append("-M")
 
@@ -154,8 +161,8 @@ class Diffable(object):
             args.append("--raw")
             args.append("-z")
 
-        # in any way, assure we don't see colored output,
-        # fixes https://github.com/gitpython-developers/GitPython/issues/172
+        # Ensure we never see colored output.
+        # Fixes: https://github.com/gitpython-developers/GitPython/issues/172
         args.append("--no-color")
 
         if paths is not None and not isinstance(paths, (tuple, list)):
@@ -168,17 +175,17 @@ class Diffable(object):
         if other is self.Index:
             args.insert(0, "--cached")
         elif other is NULL_TREE:
-            args.insert(0, "-r")  # recursive diff-tree
+            args.insert(0, "-r")  # Recursive diff-tree.
             args.insert(0, "--root")
             diff_cmd = self.repo.git.diff_tree
         elif other is not None:
-            args.insert(0, "-r")  # recursive diff-tree
+            args.insert(0, "-r")  # Recursive diff-tree.
             args.insert(0, other)
             diff_cmd = self.repo.git.diff_tree
 
         args.insert(0, self)
 
-        # paths is list here or None
+        # paths is list here, or None.
         if paths:
             args.append("--")
             args.extend(paths)
@@ -198,13 +205,13 @@ T_Diff = TypeVar("T_Diff", bound="Diff")
 
 
 class DiffIndex(List[T_Diff]):
+    """An Index for diffs, allowing a list of Diffs to be queried by the diff
+    properties.
 
-    """Implements an Index for diffs, allowing a list of Diffs to be queried by
-    the diff properties.
+    The class improves the diff handling convenience.
+    """
 
-    The class improves the diff handling convenience"""
-
-    # change type invariant identifying possible ways a blob can have changed
+    # Change type invariant identifying possible ways a blob can have changed:
     # A = Added
     # D = Deleted
     # R = Renamed
@@ -215,10 +222,10 @@ class DiffIndex(List[T_Diff]):
     def iter_change_type(self, change_type: Lit_change_type) -> Iterator[T_Diff]:
         """
         :return:
-            iterator yielding Diff instances that match the given change_type
+            iterator yielding :class:`Diff` instances that match the given `change_type`
 
         :param change_type:
-            Member of DiffIndex.change_type, namely:
+            Member of :attr:`DiffIndex.change_type`, namely:
 
             * 'A' for added paths
             * 'D' for deleted paths
@@ -246,11 +253,10 @@ class DiffIndex(List[T_Diff]):
 
 
 class Diff(object):
-
     """A Diff contains diff information between two Trees.
 
     It contains two sides a and b of the diff, members are prefixed with
-    "a" and "b" respectively to inidcate that.
+    "a" and "b" respectively to indicate that.
 
     Diffs keep information about the changed blob objects, the file mode, renames,
     deletions and new files.
@@ -273,11 +279,13 @@ class Diff(object):
 
         When comparing to working trees, the working tree blob will have a null hexsha
         as a corresponding object does not yet exist. The mode will be null as well.
-        But the path will be available though.
-        If it is listed in a diff the working tree version of the file must
-        be different to the version in the index or tree, and hence has been modified."""
+        The path will be available, though.
 
-    # precompiled regex
+        If it is listed in a diff, the working tree version of the file must
+        differ from the version in the index or tree, and hence has been modified.
+    """
+
+    # Precompiled regex.
     re_header = re.compile(
         rb"""
                                 ^diff[ ]--git
@@ -299,7 +307,8 @@ class Diff(object):
                             """,
         re.VERBOSE | re.MULTILINE,
     )
-    # can be used for comparisons
+
+    # These can be used for comparisons.
     NULL_HEX_SHA = "0" * 40
     NULL_BIN_SHA = b"\0" * 20
 
@@ -346,8 +355,8 @@ class Diff(object):
         self.a_mode = mode_str_to_int(a_mode) if a_mode else None
         self.b_mode = mode_str_to_int(b_mode) if b_mode else None
 
-        # Determine whether this diff references a submodule, if it does then
-        # we need to overwrite "repo" to the corresponding submodule's repo instead
+        # Determine whether this diff references a submodule. If it does then
+        # we need to overwrite "repo" to the corresponding submodule's repo instead.
         if repo and a_rawpath:
             for submodule in repo.submodules:
                 if submodule.path == a_rawpath.decode(defenc, "replace"):
@@ -371,7 +380,7 @@ class Diff(object):
         self.deleted_file: bool = deleted_file
         self.copied_file: bool = copied_file
 
-        # be clear and use None instead of empty strings
+        # Be clear and use None instead of empty strings.
         assert raw_rename_from is None or isinstance(raw_rename_from, bytes)
         assert raw_rename_to is None or isinstance(raw_rename_to, bytes)
         self.raw_rename_from = raw_rename_from or None
@@ -395,15 +404,15 @@ class Diff(object):
         return hash(tuple(getattr(self, n) for n in self.__slots__))
 
     def __str__(self) -> str:
-        h: str = "%s"
+        h = "%s"
         if self.a_blob:
             h %= self.a_blob.path
         elif self.b_blob:
             h %= self.b_blob.path
 
-        msg: str = ""
-        line = None  # temp line
-        line_length = 0  # line length
+        msg = ""
+        line = None
+        line_length = 0
         for b, n in zip((self.a_blob, self.b_blob), ("lhs", "rhs")):
             if b:
                 line = "\n%s: %o | %s" % (n, b.mode, b.hexsha)
@@ -414,7 +423,7 @@ class Diff(object):
             msg += line
         # END for each blob
 
-        # add headline
+        # Add headline.
         h += "\n" + "=" * line_length
 
         if self.deleted_file:
@@ -437,11 +446,7 @@ class Diff(object):
             msg += "\n---"
         # END diff info
 
-        # Python2 silliness: have to assure we convert our likely to be unicode object to a string with the
-        # right encoding. Otherwise it tries to convert it using ascii, which may fail ungracefully
-        res = h + msg
-        # end
-        return res
+        return h + msg
 
     @property
     def a_path(self) -> Optional[str]:
@@ -461,8 +466,11 @@ class Diff(object):
 
     @property
     def renamed(self) -> bool:
-        """:returns: True if the blob of our diff has been renamed
-        :note: This property is deprecated, please use ``renamed_file`` instead.
+        """
+        :returns: True if the blob of our diff has been renamed
+
+        :note: This property is deprecated.
+            Please use the :attr:`renamed_file` property instead.
         """
         return self.renamed_file
 
@@ -493,17 +501,17 @@ class Diff(object):
         :return: git.DiffIndex
         """
 
-        ## FIXME: Here SLURPING raw, need to re-phrase header-regexes linewise.
+        # FIXME: Here SLURPING raw, need to re-phrase header-regexes linewise.
         text_list: List[bytes] = []
         handle_process_output(proc, text_list.append, None, finalize_process, decode_streams=False)
 
-        # for now, we have to bake the stream
+        # For now, we have to bake the stream.
         text = b"".join(text_list)
         index: "DiffIndex" = DiffIndex()
         previous_header: Union[Match[bytes], None] = None
         header: Union[Match[bytes], None] = None
-        a_path, b_path = None, None  # for mypy
-        a_mode, b_mode = None, None  # for mypy
+        a_path, b_path = None, None  # For mypy.
+        a_mode, b_mode = None, None  # For mypy.
         for _header in cls.re_header.finditer(text):
             (
                 a_path_fallback,
@@ -532,13 +540,13 @@ class Diff(object):
             b_path = cls._pick_best_path(b_path, rename_to, b_path_fallback)
 
             # Our only means to find the actual text is to see what has not been matched by our regex,
-            # and then retro-actively assign it to our index
+            # and then retro-actively assign it to our index.
             if previous_header is not None:
                 index[-1].diff = text[previous_header.end() : _header.start()]
             # end assign actual diff
 
-            # Make sure the mode is set if the path is set. Otherwise the resulting blob is invalid
-            # We just use the one mode we should have parsed
+            # Make sure the mode is set if the path is set. Otherwise the resulting blob is invalid.
+            # We just use the one mode we should have parsed.
             a_mode = old_mode or deleted_file_mode or (a_path and (b_mode or new_mode or new_file_mode))
             b_mode = b_mode or new_mode or new_file_mode or (b_path and a_mode)
             index.append(
@@ -579,7 +587,7 @@ class Diff(object):
 
         for line in lines.split("\x00:"):
             if not line:
-                # The line data is empty, skip
+                # The line data is empty, skip.
                 continue
             meta, _, path = line.partition("\x00")
             path = path.rstrip("\x00")
@@ -603,7 +611,7 @@ class Diff(object):
             rename_to = None
 
             # NOTE: We cannot conclude from the existence of a blob to change type
-            # as diffs with the working do not have blobs yet
+            # as diffs with the working do not have blobs yet.
             if change_type == "D":
                 b_blob_id = None  # Optional[str]
                 deleted_file = True
@@ -621,7 +629,7 @@ class Diff(object):
                 b_path = b_path_str.encode(defenc)
                 rename_from, rename_to = a_path, b_path
             elif change_type == "T":
-                # Nothing to do
+                # Nothing to do.
                 pass
             # END add/remove handling
 

commit 30f7da508154df8b968f171b789c366c52276cee

Fix erroneous reference to DateTime "class"
DateTime is not a class here, and the parameter is expected as int.
This fixes a documentation bug I introduced in cd16a35 (#1725).

git/objects/tag.py
@@ -68,7 +68,7 @@ class TagObject(base.Object):
             :class:`~git.util.Actor` identifying the tagger.
 
         :param tagged_date: int_seconds_since_epoch
-            The :class:`DateTime` of the tag creation.
+            The DateTime of the tag creation.
             Use :func:`time.gmtime` to convert it into a different format.
 
         :param tagger_tz_offset: int_seconds_west_of_utc