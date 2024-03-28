e3728c3dca9cac2b49f827dbf5d1a100602f33d9
Eliah Kagan <degeneracypressure@gmail.com>
2024-03-27 20:54:50 -0400

    Decompose new fixture logic better
    This will be even more helpful when testing a deprecated member of
    the Commit class (not yet done).

--- test/deprecation/test_various.py
+++ test/deprecation/test_various.py
@@ -13,28 +13,33 @@ from git.repo import Repo
 
 
 @pytest.fixture
-def single_diff(tmp_path):
-    """Fixture to supply a single-file diff."""
-    # TODO: Consider making a fake diff rather than using a real repo and commit.
+def commit(tmp_path):
+    """Fixture to supply a one-commit repo's commit, enough for deprecation tests."""
     (tmp_path / "a.txt").write_text("hello\n", encoding="utf-8")
     repo = Repo.init(tmp_path)
     repo.index.add(["a.txt"])
-    commit = repo.index.commit("Initial commit")
-    (diff,) = commit.diff(NULL_TREE)  # Exactly one file in the diff.
-    yield diff
-    del repo, commit, diff
+    yield repo.index.commit("Initial commit")
+    del repo
     gc.collect()
 
 
-def test_diff_renamed_warns(single_diff):
+@pytest.fixture
+def diff(commit):
+    """Fixture to supply a single-file diff."""
+    # TODO: Consider making a fake diff rather than using a real repo and commit.
+    (diff,) = commit.diff(NULL_TREE)  # Exactly one file in the diff.
+    yield diff
+
+
+def test_diff_renamed_warns(diff):
     """The deprecated Diff.renamed property issues a deprecation warning."""
     with pytest.deprecated_call():
-        single_diff.renamed
+        diff.renamed
 
 
-def test_diff_renamed_file_does_not_warn(single_diff):
+def test_diff_renamed_file_does_not_warn(diff):
     """The preferred Diff.renamed_file property issues no deprecation warning."""
     with warnings.catch_warnings():
         # FIXME: Refine this to filter for deprecation warnings from GitPython.
         warnings.simplefilter("error", DeprecationWarning)
-        single_diff.renamed_file
+        diff.renamed_file
