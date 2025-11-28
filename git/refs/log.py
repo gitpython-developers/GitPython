086e83239388988772e21ee820c23e59533382f7
George Ogden <george-ogden@outlook.com>
2025-11-28 21:48:38 +0000

    Fix union type conversion to path

--- git/refs/log.py
+++ git/refs/log.py
@@ -168,7 +168,7 @@ class RefLog(List[RefLogEntry], Serializable):
         """Initialize this instance with an optional filepath, from which we will
         initialize our data. The path is also used to write changes back using the
         :meth:`write` method."""
-        self._path = os.fspath(filepath)
+        self._path = None if filepath is None else os.fspath(filepath)
         if filepath is not None:
             self._read_from_file()
         # END handle filepath
